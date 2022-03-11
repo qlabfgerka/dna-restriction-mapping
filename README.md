a515ecb7d7ca9c4356bad3ff320766fb440292cd
Jan Premzl <jan.premzl16@gmail.com>
2022-03-11 18:49:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dna-restriction-mapping
+ 
---
+++ src/main/java/inc/premzl/DNARestrictionMapping.java
@@ -0,0 +1,7 @@
+package inc.premzl;
+
+public class DNARestrictionMapping {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
