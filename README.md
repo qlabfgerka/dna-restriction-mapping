3fab797ba9fd8594a3a2ff69de441e7296d65451
Jan Premzl <jan.premzl16@gmail.com>
2022-03-12 18:51:05 +0100

    Added measurements and README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# dna-restriction-mapping
+# DNA Restriction Mapping
+
+**Application for finding restriction cuts, calculating multi sets and reconstructing restriction cuts**
+
+## Configuration
+
+```
+    dna-file algorithm export [cuts]
+    
+    dna-file: path to the DNA file
+    algorithm: b - brute force
+               bnb - branch and bound
+               bTest - brute force measurements
+               bnbTest - branch and bound measurements
+    export: y - export to file
+            else dont export
+    [cuts]: array of restriction cuts 
+    
+    Example: assets\dna\dna1.txt b y TTCC CTCTCT
+```
  
--- src/main/java/inc/premzl/DNARestrictionMapping.java
+++ src/main/java/inc/premzl/DNARestrictionMapping.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static inc.premzl.DNA.Reconstruction.bruteForce;
 import static inc.premzl.DNA.Reconstruction.partialDigest;
@@ -26,6 +27,20 @@ public class DNARestrictionMapping {
         } else if (Objects.equals(args[1], "bnb")) {
             list = partialDigest(multiset);
             printListOfLists(list);
+        } else if (Objects.equals(args[1], "bTest") || Objects.equals(args[1], "bnbTest")) {
+            System.out.println(calculateSize(multiset.size()) - 2);
+            long avgTime = 0, start, end;
+            int iterations = 10000;
+            for (int i = 0; i < iterations; i++) {
+                start = System.nanoTime();
+                if (Objects.equals(args[1], "bTest")) bruteForce(multiset, calculateSize(multiset.size()));
+                else partialDigest(multiset);
+                end = System.nanoTime();
+                avgTime += (end - start);
+                multiset = findMultiset(handleIndexes(Arrays.copyOfRange(args, 3, args.length), file));
+            }
+            System.out.println((double) TimeUnit.MILLISECONDS.convert(avgTime, TimeUnit.NANOSECONDS) / iterations);
+            return;
         } else throw new Exception("Invalid algorithm argument");
 
         if (Objects.equals(args[2], "y")) {
